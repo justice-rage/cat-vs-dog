2db51f8a02cc470bbe00f7ccac4dbf43fa346a03
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-23 19:02:43 -0800

    add cat and dog noise response

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
-    $("ul#user").children("li").first().click(function() {
-      $(this).remove();
-    });
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-    });
-  });
-
-  $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").before("<li>Bark</li>");
+    $("ul#webpage").after("<li>Meow</li>");
+    // $("ul#user").children("li").first().click(function() {
+    //   $(this).remove();
+    // });
+    // $("ul#webpage").children("li").first().click(function() {
+    //   $(this).remove();
+    // });
   });
 
   $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").before("<li>Hiss</li>");
+    $("ul#webpage").after("<li>Grrrrr</li>");
   });
 });
